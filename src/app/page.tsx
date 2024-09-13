7d2b4273bb069056c210183c6e1ac4b9aee01939
Valtkf <valentin.vgregorio@gmail.com>
2024-09-13 17:54:15 +0200

    📦 F0: Add icons file and add styling in page for the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import BasketIcon from "@/icons/BasketIcon";
+import MenuIcon from "@/icons/MenuIcon";
+import SearchIcon from "@/icons/SearchIcon";
+
 export default function Home() {
   return (
     <div className="flex justify-center font-[family-name:var(--font-geist-sans)]">
       <main className="border-[1px] border-red-500 w-full h-screen flex items-center sm:items-start">
-        <div className="flex justify-between items-center border-[1px] border-blue-500 w-full h-20 pr-10 pl-10">
-          <div className="text-2xl font-semibold">
+        <div className="flex justify-between items-center border-b-[1px] border-[#D1D1D8] w-full h-20 pl-10">
+          <div className="text-2xl font-semibold text-[#17183B]">
             <h1>Cozy®</h1>
           </div>
-          <li className="flex justify-center gap-x-4 text-black font-bold text-sm">
+          <li className="flex justify-center gap-x-10 text-[#17183B] font-bold text-sm">
             <a href="#" className="hover:underline">
               SHOP
             </a>
@@ -23,8 +27,18 @@ export default function Home() {
               CONTACT
             </a>
           </li>
-          <div>
-            <h1>logo</h1>
+          <div className="flex justify-center items-center">
+            <button className="mr-10">
+              <MenuIcon />
+            </button>
+            <button className="mr-10">
+              <SearchIcon />
+            </button>
+            <div className="border-l-[1px] border-[#D1D1D8] h-20 w-[90px] flex justify-center items-center">
+              <button>
+                <BasketIcon />
+              </button>
+            </div>
           </div>
         </div>
       </main>
